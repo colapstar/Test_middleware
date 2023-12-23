4f73a839b908e529bdb00111c39eef9cad04e932
Moran <128787888+colapstar@users.noreply.github.com>
2023-12-23 01:05:59 +0100

    tests path issues

--- automatic_run musics.bat
+++ automatic_run musics.bat
@@ -2,7 +2,7 @@
 SET OPENDIR=%cd%
 
 :: Launch API Users
-start "API Users" cmd /k "cd users && go run ./cmd/main.go"
+start "API Musics" cmd /k "cd musics_api && go run ./cmd/main.go"
 
 :: Launch Flask
 start "Flask" cmd /k "cd flask_base && set PYTHONPATH=%PYTHONPATH%;%cd% && python src/app.py"
